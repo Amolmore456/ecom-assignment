f49297f05bb998bb88f35edc77529ec87c5a55d2
Amolmore456 <amolmore456@gmail.com>
2021-02-12 15:36:31 +0530

    header css fix

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,11 @@
-import { Navbar, Nav, NavDropdown, Badge } from 'react-bootstrap'
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Badge,
+  Button,
+  Container,
+} from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { Route } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
@@ -20,53 +27,66 @@ const Header = () => {
   return (
     <>
       <Navbar bg='light' expand='lg' collapseOnSelect>
-        <LinkContainer to='/'>
-          <Navbar.Brand>Dev Swag</Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Route render={({ history }) => <SearchBox history={history} />} />
-          <Nav className='ml-auto'>
-            <LinkContainer to='/cart'>
-              <Nav.Link>
-                {cartItems.length}
-                <i className='fas fa-shopping-cart'></i>
-                Cart
-              </Nav.Link>
-            </LinkContainer>
-            {userInfo ? (
-              <NavDropdown title={userInfo.name} id='username'>
-                <LinkContainer to='/profile'>
-                  <NavDropdown.Item>Profile</NavDropdown.Item>
-                </LinkContainer>
-                <NavDropdown.Item onClick={logoutHandler}>
-                  Logout
-                </NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              <LinkContainer to='/login'>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Dev Swag</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Route render={({ history }) => <SearchBox history={history} />} />
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
                 <Nav.Link>
-                  <i className='fas fa-user'></i> Sign In
+                  <i className='fas fa-shopping-cart'></i>
+                  <span className='ml-1'>Cart</span>
                 </Nav.Link>
               </LinkContainer>
+              <span
+                style={{
+                  width: '25px',
+                  height: '25px',
+                  backgroundColor: '#CC0000',
+                  color: '#ffffff',
+                  borderRadius: '50%',
+                  textAlign: 'center',
+                }}
+              >
+                {cartItems.length}
+              </span>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+            {userInfo && userInfo.isAdmin ? (
+              <NavDropdown title='Admin' id='adminmenu'>
+                <LinkContainer to='/admin/userlist'>
+                  <NavDropdown.Item>Users</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/admin/productlist'>
+                  <NavDropdown.Item>Product</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/admin/orderlist'>
+                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+            ) : (
+              <></>
             )}
-          </Nav>
-          {userInfo && userInfo.isAdmin ? (
-            <NavDropdown title='Admin' id='adminmenu'>
-              <LinkContainer to='/admin/userlist'>
-                <NavDropdown.Item>Users</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to='/admin/productlist'>
-                <NavDropdown.Item>Product</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to='/admin/orderlist'>
-                <NavDropdown.Item>Orders</NavDropdown.Item>
-              </LinkContainer>
-            </NavDropdown>
-          ) : (
-            <></>
-          )}
-        </Navbar.Collapse>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   )

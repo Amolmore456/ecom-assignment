35c80204a7756ea59d4c9e5fcae18e083b1d8b9a
Amolmore456 <amolmore456@gmail.com>
2021-02-15 14:07:21 +0530

    product image upload fix

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -5,24 +5,22 @@ import multer from 'multer'
 import path from 'path'
 
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
+  destination: function (req, file, cb) {
     cb(null, 'uploads/')
   },
-  filename(req, file, cb) {
+  filename: function (req, file, cb) {
     cb(
       null,
-      `${file.filename}-${Date.now()}.${path.extname(file.originalname)}`
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
     )
   },
 })
 
 function checkFileType(file, cb) {
   const fileTypes = /jpg|jpeg|png/
-  const extname = fileTypes.test(
-    path.extname(file.originalname).toLocaleLowerCase()
-  )
-  const mimetypes = fileTypes.test(file.mimetypes)
-  if (extname && mimetypes) {
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+  const mimetype = fileTypes.test(file.mimetype)
+  if (extname && mimetype) {
     return cb(null, true)
   } else {
     cb('Images only!')
@@ -30,16 +28,15 @@ function checkFileType(file, cb) {
 }
 
 const upload = multer({
-  // storage: storage,
-  // fileFilter: function (req, file, cb) {
-  //   checkFileType(file, cb)
-  // },
-  dest: 'uploads/',
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb)
+  },
 })
 
 router.post('/', upload.single('image'), (req, res) => {
   console.log(req.file)
-  res.send(`/images/${req.file.filename}.jpg`)
+  res.send(`/${req.file.path}`)
 })
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import productRotues from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-import testRoutes from './routes/testRoutes.js'
 
 dotenv.config()
 const app = express()
@@ -23,13 +22,13 @@ app.use('/api/upload', uploadRoutes)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
-  app.use('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+  app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
 } else {
-  app.get('/',(req,res)=>{
+  app.get('/', (req, res) => {
     res.send('Api is running....')
   })
 }
@@ -41,7 +40,6 @@ app.use((err, req, res, next) => {
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   })
 })
-
 
 const PORT = process.env.PORT || 8000
 app.listen(
